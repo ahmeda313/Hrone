ac953da88b932c1fa588c4ed021e5731b2a73504
ahmeda313 <syedahmeda313@gmail.com>
2024-10-17 14:48:48 +0530

    frontend url error

--- Frontend/src/components/LoginPage.jsx
+++ Frontend/src/components/LoginPage.jsx
@@ -98,7 +98,7 @@ export async function action({params, request}){
 
   // console.log(formToJSON)
 
-  const res = await fetch(import.meta.env.VITE_API_URL/+"/login",{
+  const res = await fetch(import.meta.env.VITE_API_URL+"/login",{
     method:"POST",
     credentials: 'include',
     body:JSON.stringify(formToJSON),
